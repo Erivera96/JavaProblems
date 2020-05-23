897718d0953d416f5a05fea9df27343450d9f3fb
Emily Rivera <emrivera@kean.edu>
2020-05-22 20:55:41 -0400

    Update and rename UglyNumber.c to LC263_UglyNumber.c

--- LC263_UglyNumber.c
+++ LC263_UglyNumber.c
@@ -1,3 +1,6 @@
+// LeetCode # 263: Write a program to check whether a given number is an ugly number.
+// Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
 #include <stdio.h>
 
 int main()
